bf7a80a37562c528ce23164fad942c6baa9f5e8e
skullcrushercmd <skullcrushers9809@gmail.com>
2023-12-04 01:30:45 -0500

    need to fix styles,idea of title having glowblue

---
+++ animations.js
@@ -0,0 +1,26 @@
+var githubAnimation = lottie.loadAnimation({
+    container: document.getElementById('github'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/content/github.json'
+});
+var youtubeAnimation = lottie.loadAnimation({
+    container: document.getElementById('youtube'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    path: '/content/youtube.json'
+});
+document.getElementById('github').addEventListener('mouseover', function() {
+    githubAnimation.play();
+});
+document.getElementById('github').addEventListener('mouseout', function() {
+    githubAnimation.stop();
+});
+document.getElementById('youtube').addEventListener('mouseover', function() {
+    youtubeAnimation.play();
+});
+document.getElementById('youtube').addEventListener('mouseout', function() {
+    youtubeAnimation.stop();
+});
