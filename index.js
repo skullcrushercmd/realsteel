5d7c8f05d8ee0416741a9c5e2bfb16c2915ff936
skullcrushercmd <skullcrushers9809@gmail.com>
2023-12-11 23:18:38 -0500

    minors

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var totalScroll = 0;
 
 window.addEventListener('wheel', function(e) {
-    
+    console.log('Wheel event fired', e.deltaY);
     totalScroll += Math.abs(e.deltaY);
+    console.log('Total scroll', totalScroll);
 
-    
-    if (totalScroll > 1200) {
-        
+    if (totalScroll > 1400) {
+        console.log('Redirecting to es.html');
         window.location.href = 'es.html';
     }
 });
